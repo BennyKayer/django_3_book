0f8f144398cbdf3f793199044cd886d0349f4c48
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-04 16:50:40 +0200

    Dodanie do bloga wyszukiwania pełnotekstowego, instalacja postgres'a (z docker'em no bo bez przesady) 95 - 96

--- project_blog/project_blog/settings.py
+++ project_blog/project_blog/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = "project_blog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": config("DB_HOST"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASS"),
+        "PORT": config("DB_PORT"),
     }
 }
 
