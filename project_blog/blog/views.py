f237b20692927f5d74a99ac00449850cea378190
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-05-06 14:26:55 +0200

    Stemming i ranking wyników 100

--- project_blog/blog/views.py
+++ project_blog/blog/views.py
@@ -1,7 +1,11 @@
 """Views for blog application
 """
 from django.conf import settings
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -157,9 +161,22 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data["query"]
-            results = Post.objects.annotate(
-                search=SearchVector("title", "body")
-            ).filter(search=query)
+            # Szukanie po wielu hasłach
+            # results = Post.objects.annotate(
+            #     search=SearchVector("title", "body")
+            # ).filter(search=query)
+
+            # Stemming
+            search_vector = SearchVector("title", "body")
+            search_query = SearchQuery(query)
+            results = (
+                Post.objects.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query),
+                )
+                .filter(search=search_query)
+                .order_by("-rank")
+            )
     return render(
         request,
         "blog/post/search.html",
